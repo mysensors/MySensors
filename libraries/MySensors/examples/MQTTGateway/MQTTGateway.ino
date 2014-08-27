724567e295236436fb84e55f6adfc486c6360072
Daniel Wiegert <daniel.wiegert@gmail.com>
2014-08-27 15:24:04 +0200

    Bugfixes and rewrite parts of code

--- libraries/MySensors/examples/MQTTGateway/MQTTGateway.ino
+++ libraries/MySensors/examples/MQTTGateway/MQTTGateway.ino
@@ -31,18 +31,19 @@ This will listen on your selected TCP_IP:TCP_PORT below, Please change TCP_MAC y
 	message has any length or not.
 	Example: if (msg.type==V_LIGHT && strlen(msg.getString())>0) otherwise the code might do strange things.
 
-* Address-layout is : [MQTT_BROKER_PREFIX]/[NodeID]/[SensorID]/[SensorType]
+* Address-layout is : [MQTT_BROKER_PREFIX]/[NodeID]/[SensorID]/V_[SensorType]
 	NodeID and SensorID is uint8 (0-255) number.
 	Last segment is translation of the sensor type, look inside MyMQTT.cpp for the definitions.
 	User can change this to their needs. We have also left some space for custom types.
 
 Special: (sensor 255 reserved for special commands)
-You can receive a node sketch name with MyMQTT/20/255/Sketch_name (or version with _version)
+You can receive a node sketch name with MyMQTT/20/255/V_Sketch_name (or version with _version)
 
 To-do:
 Special commands : clear or set EEPROM Values, Send REBOOT and Receive reboot for MyMQTT itself.
 Be able to send ACK so client returns the data being sent.
 ... Please come with ideas!
+What to do with publish messages.
 
 Test in more MQTT clients, So far tested in openhab and MyMQTT for Android (Not my creation)
 - http://www.openhab.org/
@@ -50,51 +51,16 @@ Test in more MQTT clients, So far tested in openhab and MyMQTT for Android (Not 
 ... Please notify me if you use this broker with other software.
 
 
-* Example Openhab configuration:
-openhab.cfg
----
-mqtt:mysensor.url=tcp://192.168.0.234:1883
-mqtt:mysensor.clientId=MQTT
----
-
-items/test.items
----
-Group test
-Number Temp_test	"Temp [%.1f °C]"		(test)	{mqtt="<[mysensor:MyMQTT/20/#/Temperature:state:default]"}
-Number Hum_test		"Hum [%.1f %%]"			(test)	{mqtt="<[mysensor:MyMQTT/20/#/Humidity:state:default]"}
-Number test_test	"test [%s]"				(test)	{mqtt="<[mysensor:MyMQTT/20/3/#:state:default]"}
-Switch sw1			"sw 1"     				(test)	{mqtt="<[mysensor:MyMQTT/21/1/Light:command:MAP(1on0off.map)]"}
-Switch sw2	"sw2 recieve example"			(test) 	{mqtt=">[mysensor:MyMQTT/21/2/Light:command:ON:1],
-	>[mysensor:MyMQTT/21/2/Light:command:OFF:0]"}
-Switch sw3	"sw2 send + recieve example"	(test) 	{mqtt=">[mysensor:MyMQTT/21/2/Light:command:ON:1],
-	>[mysensor:MyMQTT/21/2/Light:command:OFF:0],<[mysensor:MyMQTT/21/2/Light:command:MAP(1on0off.map)]"}
-String sketch20		"Node 20 Sketch name [%s]"	(test)	{mqtt="<[mysensor:MyMQTT/20/255/Sketch_name:state:default]"}
-String sketch21 	"Node 21 Sketch name [%s]"	(test)	{mqtt="<[mysensor:MyMQTT/21/255/Sketch_name:state:default]"}
---- (Note; # = Wildcard character)
-
-sitemap/test.site
----
-sitemap demo label="Menu"
-Frame label="Openhab" {
-		Group item=test label="Test group"
-}
----
-
-transform/1on0off.map
----
-1=ON
-0=OFF
----
+* How to set-up Openhab and MQTTGateway:
+http://forum.mysensors.org/topic/303/mqtt-broker-gateway
 
 */
-
 
 #include <SPI.h>
 #include <MyMQTT.h>
 #include <Ethernet.h>
 
-
-// Use this for IBOARD modded to use standard MISO/MOSI/SCK, see note *1 above!
+// * Use this for IBOARD modded to use standard MISO/MOSI/SCK, see note *1 above!
 /*
 #define RADIO_CE_PIN        3			// radio chip enable
 #define RADIO_SPI_SS_PIN    8			// radio SPI serial select
@@ -102,7 +68,7 @@ transform/1on0off.map
 #define RADIO_RX_LED_PIN    A1  		// Receive led pin
 #define RADIO_TX_LED_PIN    A0  		// the PCB, on board LED*/
 
-//Use this for default configured pro mini / nano etc :
+// * Use this for default configured pro mini / nano etc :
 ///*
 #define RADIO_CE_PIN        5		// radio chip enable
 #define RADIO_SPI_SS_PIN    6		// radio SPI serial select
@@ -110,19 +76,18 @@ transform/1on0off.map
 #define RADIO_RX_LED_PIN    8		// Receive led pin
 #define RADIO_TX_LED_PIN    9		// the PCB, on board LED*/
 
-#define TCP_PORT 1883										// Set your MQTT Broker Listening port.
-IPAddress TCP_IP ( 192, 168, 0, 234 );						// Configure your static ip-address here
-byte TCP_MAC[] = { 0x02, 0xDE, 0xAD, 0x00, 0x00, 0x42 };	// Mac-address - You should change this! see note *2 above!
+#define TCP_PORT 1883						// Set your MQTT Broker Listening port.
+IPAddress TCP_IP ( 192, 168, 0, 234 );				// Configure your static ip-address here
+uint8_t TCP_MAC[] = { 0x02, 0xDE, 0xAD, 0x00, 0x00, 0x42 };	// Mac-address - You should change this! see note *2 above!
+
+//////////////////////////////////////////////////////////////////
 
 EthernetServer server = EthernetServer(TCP_PORT);
 MyMQTT gw(RADIO_CE_PIN, RADIO_SPI_SS_PIN);
 
-// -- Uncomment this constructor if you have leds and include button attached to your gateway
-//MyMQTT gw(RADIO_CE_PIN, RADIO_SPI_SS_PIN, RADIO_RX_LED_PIN, RADIO_TX_LED_PIN, RADIO_ERROR_LED_PIN);
-
 void processEthernetMessages() {
 	char inputString[MQTT_MAX_PACKET_SIZE] = "";
-	int inputSize = 0;
+	uint8_t inputSize = 0;
 	EthernetClient client = server.available();
 	if (client) {
 		while (client.available()) {
@@ -130,23 +95,33 @@ void processEthernetMessages() {
 			inputString[inputSize] = inChar;
 			inputSize++;
 		}
+#ifdef TCPDUMP
+		Serial.print("<<");
+		char buf[4];
+		for (uint8_t a=0; a<inputSize; a++) { sprintf(buf,"%02X ", (uint8_t)inputString[a]); Serial.print(buf); } Serial.println("");
+#endif
 		gw.processMQTTMessage(inputString, inputSize);
 	}
 }
 
-void writeEthernet(const char *writeBuffer, int *writeSize) {
-	server.write((const uint8_t *)writeBuffer, *writeSize); // Todo: Should this really be *writeSize?
+void writeEthernet(const char *writeBuffer, uint8_t *writeSize) {
+#ifdef TCPDUMP
+	Serial.print(">>");
+	char buf[4];
+	for (uint8_t a=0; a<*writeSize; a++) { sprintf(buf,"%02X ",(uint8_t)writeBuffer[a]);  Serial.print(buf); } Serial.println("");
+#endif
+	server.write((const uint8_t *)writeBuffer, *writeSize);
 }
+
 
 int main(void) {
 	init();
 	Ethernet.begin(TCP_MAC, TCP_IP);
 	delay(1000);   // Wait for Ethernet to get configured.
-	gw.begin(RF24_PA_LEVEL_GW, RF24_CHANNEL, RF24_DATARATE, writeEthernet);
+	gw.begin(RF24_PA_LEVEL_GW, RF24_CHANNEL, RF24_DATARATE, writeEthernet, RADIO_RX_LED_PIN, RADIO_TX_LED_PIN, RADIO_ERROR_LED_PIN);
 	server.begin();
 	while (1) {
 		processEthernetMessages();
 		gw.processRadioMessage();
 	}
 }
-
