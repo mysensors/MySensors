0b4f6950c7c01926a45f75f6a714ff8927a40905
Henrik Ekblad <henrik.ekblad@gmail.com>
2015-11-12 22:54:32 +0100

    NodeJS controller example moved to separate repository

--- NodeJsController/MySensors.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# pidfile must be first argument
-PIDFILE=$1
-mkdir -p /home/pi/logs
-cd /home/pi/Arduino/NodeJsController
-/opt/node/bin/node NodeJsController.js >>/home/pi/logs/NodeJsController.log 2>&1 </dev/null &
-CHILD="$!"
-echo $CHILD > $PIDFILE
