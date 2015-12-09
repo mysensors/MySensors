9ba8a9ba990275b0f29d7d430eb2feeafd1537bd
Thomas Mørch <thomas@bowmo.dk>
2015-12-09 22:02:33 +0100

    Corrected port pin number for arduino pin 0

--- hardware/MySensors/samd/variants/mysensors_gw/variant.cpp
+++ hardware/MySensors/samd/variants/mysensors_gw/variant.cpp
@@ -114,7 +114,7 @@ const PinDescription g_APinDescription[]=
   // 0..13 - Digital pins
   // ----------------------
   // 0/1 - SERCOM/UART (Serial1)
-  { PORTA,  8, PIO_SERCOM, (PIN_ATTR_DIGITAL), No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_11 }, // RX: SERCOM0/PAD[3]
+  { PORTA, 11, PIO_SERCOM, (PIN_ATTR_DIGITAL), No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_11 }, // RX: SERCOM0/PAD[3]
   { PORTA, 10, PIO_SERCOM, (PIN_ATTR_DIGITAL), No_ADC_Channel, NOT_ON_PWM, NOT_ON_TIMER, EXTERNAL_INT_10 }, // TX: SERCOM0/PAD[2]
 
   // 2..12
