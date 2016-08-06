1a597589b894b1c564d8665af24274e9b5b39a62
MartinHjelmare <marhje52@kth.se>
2016-08-06 12:04:13 +0200

    Update doxygen docs with new smartSleep behavior
    * Update docs to new behavior.
    * Add info on how to set wait time in smartSleep.
    * Fix incorrect docs regarding return value of smartSleep.

--- core/MySensorsCore.h
+++ core/MySensorsCore.h
@@ -234,7 +234,8 @@ bool wait(unsigned long ms, uint8_t cmd, uint8_t msgtype);
  */
 int8_t sleep(unsigned long ms);
 /**
-* Same as sleep(), send heartbeat upon wakeup and process incoming messages
+* Same as sleep(), send heartbeat and process incoming messages before going to sleep.
+* Specify the time to wait for incoming messages by defining MY_SMART_SLEEP_WAIT_DURATION and time in ms.
 * @param ms Number of milliseconds to sleep.
 * @return -1 if timer woke it up, -2 if not possible (e.g. ongoing FW update)
 */
@@ -251,11 +252,12 @@ int8_t smartSleep(unsigned long ms);
  */
 int8_t sleep(uint8_t interrupt, uint8_t mode, unsigned long ms=0);
 /**
-* Same as sleep(), send heartbeat upon wakeup and process incoming messages
+* Same as sleep(), send heartbeat and process incoming messages before going to sleep.
+* Specify the time to wait for incoming messages by defining MY_SMART_SLEEP_WAIT_DURATION and time in ms.
 * @param interrupt Interrupt that should trigger the wakeup
 * @param mode RISING, FALLING, CHANGE
-* @param ms Number of milliseconds to sleep.
-* @return -1 if timer woke it up, -2 if not possible (e.g. ongoing FW update)
+* @param ms Number of milliseconds to sleep or 0 to sleep forever
+* @return Interrupt number wake up was triggered by pin change, -1 if timer woke it up, -2 if not possible (e.g. ongoing FW update)
 */
 int8_t smartSleep(uint8_t interrupt, uint8_t mode, unsigned long ms=0);
 
@@ -272,7 +274,8 @@ int8_t smartSleep(uint8_t interrupt, uint8_t mode, unsigned long ms=0);
  */
 int8_t sleep(uint8_t interrupt1, uint8_t mode1, uint8_t interrupt2, uint8_t mode2, unsigned long ms=0);
 /**
-* Same as sleep(), send heartbeat upon wakeup and process incoming messages
+* Same as sleep(), send heartbeat and process incoming messages before going to sleep.
+* Specify the time to wait for incoming messages by defining MY_SMART_SLEEP_WAIT_DURATION and time in ms.
 * @param interrupt1 First interrupt that should trigger the wakeup
 * @param mode1 Mode for first interrupt (RISING, FALLING, CHANGE)
 * @param interrupt2 Second interrupt that should trigger the wakeup
