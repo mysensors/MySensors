1233bab4aca5505d1bd1dab838715f9ec11c1483
PhracturedBlue <rc2012@pblue.org>
2017-01-08 17:00:47 -0800

    Add initial support for use of STM32F1 boards

---
+++ core/MyMainSTM32F1.cpp
@@ -0,0 +1,43 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ */
+
+// Initialize library and handle sketch functions like we want to
+
+extern "C" void __libc_init_array(void);
+
+// Force init to be called *first*, i.e. before static object allocation.
+// Otherwise, statically allocated objects that need libmaple may fail.
+ __attribute__(( constructor (101))) void premain() {
+    init();
+}
+
+int main(void) {
+	_begin();
+
+	for(;;) {
+		_process();
+		if (loop) {
+			loop();
+		}
+		if (serialEventRun) {
+			serialEventRun();
+		}
+	}
+    return 0;
+}
