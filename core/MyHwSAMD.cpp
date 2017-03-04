d617fae4f3d7ef3bd2e6bbfb34bff7ac06f1077a
Thomas Mørch <thomas-nospam@bowmo.dk>
2017-03-04 12:52:43 +0100

    I2C eeprom write fix for samd

--- core/MyHwSAMD.cpp
+++ core/MyHwSAMD.cpp
@@ -41,6 +41,74 @@ ISR (WDT_vect)
 */
 
 
+void i2c_eeprom_write_buffer(int deviceaddress, unsigned int eeaddress, byte* data, byte data_len)
+{
+	// Uses Page Write for 24LC256
+	// Allows for 32 byte page boundary
+	// Splits string into max 16 byte writes
+	unsigned char i=0, counter=0;
+	unsigned int  address;
+	unsigned int  page_space;
+	unsigned int  page=0;
+	unsigned int  num_writes;
+	unsigned char first_write_size;
+	unsigned char last_write_size = 0;
+	unsigned char write_size;
+
+	// Calculate length of data
+	//do{ data_len++; } while(data[data_len]);
+
+	// Calculate space available in first page
+	page_space = int(((eeaddress/32) + 1)*32)-eeaddress;
+
+	// Calculate first write size
+	if (page_space>16) {
+		first_write_size=page_space-((page_space/16)*16);
+		if (first_write_size==0) {
+			first_write_size=16;
+		}
+	} else {
+		first_write_size=page_space;
+	}
+
+	// calculate size of last write
+	if (data_len>first_write_size) {
+		last_write_size = (data_len-first_write_size)%16;
+	}
+
+	// Calculate how many writes we need
+	if (data_len>first_write_size) {
+		num_writes = ((data_len-first_write_size)/16)+2;
+	} else {
+		num_writes = 1;
+	}
+
+	i=0;
+	address=eeaddress;
+	for(page=0; page<num_writes; page++) {
+		if(page==0) {
+			write_size=first_write_size;
+		} else if(page==(num_writes-1)) {
+			write_size=last_write_size;
+		} else {
+			write_size=16;
+		}
+
+		Wire.beginTransmission(deviceaddress);
+		Wire.write((int)((address) >> 8));   // MSB
+		Wire.write((int)((address) & 0xFF)); // LSB
+		counter=0;
+		do {
+			Wire.write((byte) data[i]);
+			i++;
+			counter++;
+		} while((data[i]) && (counter<write_size));
+		Wire.endTransmission();
+		address+=write_size;   // Increment address for next write
+
+		delay(6);  // needs 5ms for page write
+	}
+}
 
 void i2c_eeprom_write_byte(unsigned int eeaddress, byte data )
 {
@@ -50,6 +118,7 @@ void i2c_eeprom_write_byte(unsigned int eeaddress, byte data )
 	Wire.write((int)(eeaddress & 0xFF)); // LSB
 	Wire.write(rdata);
 	Wire.endTransmission();
+	delay(6)
 }
 
 byte i2c_eeprom_read_byte(unsigned int eeaddress )
@@ -79,9 +148,10 @@ void hwWriteConfigBlock(void* buf, void* adr, size_t length)
 {
 	uint8_t* src = static_cast<uint8_t*>(buf);
 	int offs = reinterpret_cast<int>(adr);
-	while (length-- > 0) {
-		i2c_eeprom_write_byte(offs++, *src++);
-	}
+	i2c_eeprom_write_buffer(I2C_EEP_ADDRESS, offs, src, length);
+	//	while (length-- > 0) {
+	//		i2c_eeprom_write_byte(offs++, *src++);
+	//	}
 }
 
 uint8_t hwReadConfig(int adr)
