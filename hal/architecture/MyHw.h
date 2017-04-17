7f4d30c1e3338be3982f51b36eb0340cdce9163f
Olivier <tekka007@users.noreply.github.com>
2017-04-17 20:33:08 +0200

    Restructure HAL files (arch) (#820)

--- hal/architecture/MyHw.h
+++ hal/architecture/MyHw.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * The MySensors Arduino library handles the wireless radio link and protocol
  * between your home built sensors/actuators and HA controller of choice.
  * The sensors forms a self healing radio network with optional repeaters. Each
