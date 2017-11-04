9663d4c6b7e896808e2f84280f7742122f967728
tekka <tekka007@users.noreply.github.com>
2017-11-04 14:38:33 +0100

    Harmonize type definitions (#970)

--- hal/architecture/MyHwESP8266.cpp
+++ hal/architecture/MyHwESP8266.cpp
@@ -89,14 +89,14 @@ ssize_t hwGetentropy(void *__buffer, size_t __length)
 	return __length;
 }
 
-int8_t hwSleep(unsigned long ms)
+int8_t hwSleep(uint32_t ms)
 {
 	// TODO: Not supported!
 	(void)ms;
 	return MY_SLEEP_NOT_POSSIBLE;
 }
 
-int8_t hwSleep(uint8_t interrupt, uint8_t mode, unsigned long ms)
+int8_t hwSleep(uint8_t interrupt, uint8_t mode, uint32_t ms)
 {
 	// TODO: Not supported!
 	(void)interrupt;
@@ -106,7 +106,7 @@ int8_t hwSleep(uint8_t interrupt, uint8_t mode, unsigned long ms)
 }
 
 int8_t hwSleep(uint8_t interrupt1, uint8_t mode1, uint8_t interrupt2, uint8_t mode2,
-               unsigned long ms)
+               uint32_t ms)
 {
 	// TODO: Not supported!
 	(void)interrupt1;
