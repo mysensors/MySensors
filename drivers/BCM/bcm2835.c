mysensors/MySensors

commit 2d5404de979498f0ad481e10b73017bd3cfd101d

CppCheck cleanup (#975)
* Add support for inline cppcheck suppressions
* Clean out all known cppcheck issues
* Terminate toll-gate on found cppcheck issues

drivers/BCM/bcm2835.c
@@ -113,11 +113,11 @@ unsigned int bcm2835_version(void)
  */
 uint32_t bcm2835_peri_read(volatile uint32_t* paddr)
 {
-	uint32_t ret;
 	if (debug) {
 		printf("bcm2835_peri_read  paddr %08X\n", (unsigned) paddr);
 		return 0;
 	} else {
+		uint32_t ret;
 		__sync_synchronize();
 		ret = *paddr;
 		__sync_synchronize();
@@ -438,7 +438,7 @@ void bcm2835_delayMicroseconds(uint64_t micros)
 
 	if (debug) {
 		/* Cant access sytem timers in debug mode */
-		printf("bcm2835_delayMicroseconds %lld\n", micros);
+		printf("bcm2835_delayMicroseconds %" PRIu64 "\n", micros);
 		return;
 	}
 

.ci/static_analysis.groovy
@@ -5,7 +5,7 @@ def cppCheck(config) {
 	sh """#!/bin/bash +x
 				cd ${config.repository_root}
 				echo "Doing cppcheck for AVR..."
-				find . -type f \\( -iname \\*.c -o -iname \\*.cpp -o -iname \\*.ino \\) | cppcheck -j 4 --file-list=- --enable=style,information --platform=.mystools/cppcheck/config/avr.xml --suppressions-list=.mystools/cppcheck/config/suppressions.cfg --includes-file=.mystools/cppcheck/config/includes.cfg --language=c++ --xml --xml-version=2 2> cppcheck-avr.xml
+				find . -type f \\( -iname \\*.c -o -iname \\*.cpp -o -iname \\*.ino \\) | cppcheck -j 4 --force --file-list=- --enable=style,information --platform=.mystools/cppcheck/config/avr.xml --suppressions-list=.mystools/cppcheck/config/suppressions.cfg --includes-file=.mystools/cppcheck/config/includes.cfg --language=c++ --inline-suppr --xml --xml-version=2 2> cppcheck-avr.xml
 				cppcheck-htmlreport --file="cppcheck-avr.xml" --title="cppcheck-avr" --report-dir=cppcheck-avr_cppcheck_reports --source-dir=."""
 	
 	publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true,
@@ -29,9 +29,8 @@ def cppCheck(config) {
 				"grep -q \"<td>0</td><td>total</td>\" cppcheck-avr_cppcheck_reports/index.html || exit_code=\$?\n"+
 				"exit \$((exit_code == 0 ? 0 : 1))")
 	if (ret == 1) {
-		config.pr.setBuildStatus(config, 'SUCCESS', 'Toll gate (Code analysis - Cppcheck)', 'Issues found (but are not considered critical)', '${BUILD_URL}CppCheck_AVR/index.html')
-		//currentBuild.result = 'UNSTABLE'
-		//error 'Terminating due to Cppcheck error'
+		config.pr.setBuildStatus(config, 'ERROR', 'Toll gate (Code analysis - Cppcheck)', 'Issues found', '${BUILD_URL}CppCheck_AVR/index.html')
+		error 'Terminating due to Cppcheck error'
 	} else {
 		config.pr.setBuildStatus(config, 'SUCCESS', 'Toll gate (Code analysis - Cppcheck)', 'Pass', '')
 	}

.clang_complete
@@ -1,8 +1,3 @@
--IC:\Program Files (x86)\Arduino\hardware\arduino\avr\cores\arduino
--IC:\Program Files (x86)\Arduino\hardware\tools\avr\avr\include
 -Idrivers\Linux
 -Idrivers\ATSHA204
 -Icore
--DMY_SIGNING_ATSHA204
--DMY_SIGNING_SOFT
--DARDUINO_ARCH_AVR

.mystools/cppcheck/config/suppressions.cfg
@@ -1,5 +1,4 @@
-toomanyconfigs
-missingInclude
-missingIncludeSystem
 ConfigurationNotChecked
 unmatchedSuppression
+// This suppression is because the problem is in an in-lined macro so in-line-suppressions does not appear to take effect
+unreadVariable:*/MyHwNRF5.cpp

.mystools/cppcheck/options.sh
@@ -9,6 +9,7 @@ OPTIONS="--quiet                                      \
 	--library=${LIBRARY:-avr}                           \
 	--platform="${TOOLCONFIG}"/${PLATFORM:-avr.xml}     \
  	--includes-file="${TOOLCONFIG}"/includes.cfg	      \
+ 	--inline-suppr                                      \
 	--suppressions-list="${TOOLCONFIG}"/suppressions.cfg"
 
 echo $OPTIONS

core/MyGatewayTransportEthernet.cpp
@@ -49,7 +49,11 @@ MyMessage _ethernetMsg;
 #define ARRAY_SIZE(x)  (sizeof(x)/sizeof(x[0]))
 
 typedef struct {
+	// Suppress the warning about unused members in this struct because it is used through a complex
+	// set of preprocessor directives
+	// cppcheck-suppress unusedStructMember
 	char string[MY_GATEWAY_MAX_RECEIVE_LENGTH];
+	// cppcheck-suppress unusedStructMember
 	uint8_t idx;
 } inputBuffer;
 

core/MyLeds.cpp
@@ -58,7 +58,9 @@ void ledsProcess()
 	}
 	prevTime = hwMillis();
 
+#if defined(MY_DEFAULT_RX_LED_PIN) || defined(MY_DEFAULT_TX_LED_PIN) || defined(MY_DEFAULT_ERR_LED_PIN)
 	uint8_t state;
+#endif
 
 	// For an On/Off ratio of 4, the pattern repeated will be [on, on, on, off]
 	// until the counter becomes 0.

core/MyOTAFirmwareUpdate.cpp
@@ -128,7 +128,6 @@ bool firmwareOTAUpdateProcess(void)
 					MY_SERIALDEVICE.print(prbuf);
 				}
 				OTA_DEBUG(PSTR("\n"));
-				8
 			}
 #endif
 			_firmwareBlock--;

commit 0b6a4baeaf83ec656a4af92218dffa505b0316a0

BCM driver: include inttypes
This makes it possible to compile the Raspberry Pi gateway on
Raspbian 9 (stretch), even without adding
--extra-cxxflags="-D__STDC_FORMAT_MACROS"
(as was suggested in
https://github.com/mysensors/MySensors/issues/955 but was
insufficient for remediating this problem)

The compile problem was inadvertently introduced in
2d5404de9.

This change fixes the problem discussed in
https://github.com/mysensors/MySensors/issues/980

drivers/BCM/bcm2835.c
@@ -20,6 +20,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <inttypes.h> // For PRIu64
 #include "log.h"
 
 #define BCK2835_LIBRARY_BUILD