8db8ee9cbd05664d2fcf78b17b862a5dd64b9368
Mikael Falkvidd <github@mjo.se>
2017-11-17 21:18:15 +0100

    Update RelayActuator example (#973)
    Based on feedback from skywatch,
    https://forum.mysensors.org/post/78896
    
    We'll need to update the wiring instructions on
    https://www.mysensors.org/build/relay as well.

--- examples/RelayActuator/RelayActuator.ino
+++ examples/RelayActuator/RelayActuator.ino
@@ -41,7 +41,7 @@
 
 #include <MySensors.h>
 
-#define RELAY_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define RELAY_PIN 4  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
 #define NUMBER_OF_RELAYS 1 // Total number of attached relays
 #define RELAY_ON 1  // GPIO value to write to turn on attached relay
 #define RELAY_OFF 0 // GPIO value to write to turn off attached relay
@@ -49,7 +49,7 @@
 
 void before()
 {
-	for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
+	for (int sensor=1, pin=RELAY_PIN; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
 		// Then set relay pins in output mode
 		pinMode(pin, OUTPUT);
 		// Set relay to last known state (using eeprom storage)
@@ -67,7 +67,7 @@ void presentation()
 	// Send the sketch version information to the gateway and Controller
 	sendSketchInfo("Relay", "1.0");
 
-	for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
+	for (int sensor=1, pin=RELAY_PIN; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
 		// Register all sensors to gw (they will be created as child devices)
 		present(sensor, S_BINARY);
 	}
@@ -84,7 +84,7 @@ void receive(const MyMessage &message)
 	// We only expect one type of message from controller. But we better check anyway.
 	if (message.type==V_STATUS) {
 		// Change relay state
-		digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
+		digitalWrite(message.sensor-1+RELAY_PIN, message.getBool()?RELAY_ON:RELAY_OFF);
 		// Store state in eeprom
 		saveState(message.sensor, message.getBool());
 		// Write some debug info
