9444112501984e9df50c8795ba9245b0dfdca2f5
Patrick Fallberg <patrick@fallberg.net>
2018-01-25 21:09:51 +0100

    Various CI-pipeline fixes
    * Let Gitler produce a changelog for release builds
    * Fix bug in publishing release documentation
    * Fix bug in labeling documentation
       (now uses tag data for better tracability)
    * Update copyright in documentation

--- .ci/gitler.groovy
+++ .ci/gitler.groovy
@@ -15,7 +15,41 @@ def call(config) {
 	else if (env.CHANGE_TARGET == 'master')
 	{
 		echo env.CHANGE_AUTHOR + ' is a valid author for targeting master branch, skipping further validation'
+		dir(config.repository_root) {
+			step([$class: 'GitChangelogRecorder', config: [configFile: 'git-changelog-settings.json',
+				createFileTemplateContent: '''
+# Changelog
+{{#commits}}
+### {{{messageTitle}}}
+{{{messageBody}}}
+[{{hash}}](https://github.com/mysensors/MySensors/commit/{{hash}}) by {{authorName}} at *{{commitTime}}*
+{{/commits}}
+''',
+				createFileTemplateFile: '', createFileUseTemplateContent: true,
+				createFileUseTemplateFile: false, customIssues: [[link: '', name: '', pattern: '', title: ''],
+				[link: '', name: '', pattern: '', title: '']], dateFormat: 'YYYY-MM-dd HH:mm:ss',
+				file: 'ReleaseNotes.md', fromReference: env.CHANGE_TARGET, fromType: 'ref', gitHubApi: '',
+				gitHubApiTokenCredentialsId: '', gitHubIssuePattern: '#([0-9]+)', gitHubToken: '',
+				gitLabApiTokenCredentialsId: '', gitLabProjectName: '', gitLabServer: '', gitLabToken: '',
+				ignoreCommitsIfMessageMatches: '^Merge.*',
+				ignoreCommitsWithoutIssue: false, ignoreTagsIfNameMatches: '',
+				jiraIssuePattern: '\\b[a-zA-Z]([a-zA-Z]+)-([0-9]+)\\b', jiraPassword: '', jiraServer: '',
+				jiraUsername: '', jiraUsernamePasswordCredentialsId: '', mediaWikiPassword: '',
+				mediaWikiTemplateContent: '',	mediaWikiTemplateFile: '', mediaWikiTitle: '', mediaWikiUrl: '',
+				mediaWikiUseTemplateContent: false, mediaWikiUseTemplateFile: false, mediaWikiUsername: '',
+				noIssueName: 'No issue', readableTagName: '/([^/]+?)$', showSummary: false,
+				showSummaryTemplateContent: '', showSummaryTemplateFile: '', showSummaryUseTemplateContent: false,
+				showSummaryUseTemplateFile: false, subDirectory: '', timeZone: 'UTC',
+				toReference: config.git_sha, toType: 'commit', untaggedName: 'Unreleased',
+				useConfigFile: false, useFile: true, useGitHub: true, useGitHubApiTokenCredentials: false,
+				useGitLab: false, useGitLabApiTokenCredentials: false, useIgnoreTagsIfNameMatches: false,
+				useJira: false, useJiraUsernamePasswordCredentialsId: false, useMediaWiki: false,
+				useReadableTagName: false, useSubDirectory: false]
+			])
+		}
+
 		config.pr.setBuildStatus(config, 'SUCCESS', 'Toll gate (Gitler)', 'Pass', '')
+		config.pr.setBuildStatus(config, 'SUCCESS', 'Toll gate (Release changelog)', '', '${BUILD_URL}execution/node/3/ws/MySensors/ReleaseNotes.md/*view*/')
 		return
 	}
 
