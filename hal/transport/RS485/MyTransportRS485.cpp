6ff0c1d4665745544e94809a750e117444839a9a
Patrick Fallberg <patrick@fallberg.net>
2018-06-16 11:03:08 +0200

    Strip even more trailing spaces

--- hal/transport/RS485/MyTransportRS485.cpp
+++ hal/transport/RS485/MyTransportRS485.cpp
@@ -155,7 +155,7 @@ bool _serialProcess()
 				_recPhase = 1;
 				_recPos = 0;
 
-				//Avoid _data[] overflow 
+				//Avoid _data[] overflow
 				if (_recLen >= MY_RS485_MAX_MESSAGE_LENGTH) {
 					_serialReset();
 					break;
