1c33609556e10b15079d8d7452cd1355457bb147
Marcelo Aquino <marceloaqno@users.noreply.github.com>
2018-09-04 03:59:32 -0300

    Fix SPIDEV on Armbian Stretch (#1200)
    Fix #1195

--- drivers/BCM/BCM.h
+++ drivers/BCM/BCM.h
@@ -38,6 +38,12 @@ public:
 	 */
 	~BCMClass();
 	/**
+	 * @brief Initializes BCM.
+	 *
+	 * @return 1 if successful, else exits the program.
+	 */
+	uint8_t init();
+	/**
 	 * @brief Configures the specified pin to behave either as an input or an output.
 	 *
 	 * @param gpio The GPIO pin number.
@@ -65,6 +71,12 @@ public:
 	 * @return The GPIO pin number.
 	 */
 	inline uint8_t digitalPinToInterrupt(uint8_t gpio);
+	/**
+	 * @brief Checks if SPI was initialized.
+	 *
+	 * @return 1 if initialized, else 0.
+	 */
+	uint8_t isInitialized();
 
 private:
 	static uint8_t initialized; //!< @brief BCM initialized flag.
