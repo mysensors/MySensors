23adb6f00462e63cb8515fb7f50c0afa584386eb
KooLru <kool@kool.ru>
2020-05-24 00:38:01 +0300

    Cleanup code

--- hal/architecture/STM32/MyHwSTM32.cpp
+++ hal/architecture/STM32/MyHwSTM32.cpp
@@ -41,20 +41,8 @@ bool hwInit(void)
 	while (!MY_SERIALDEVICE) {}
 #endif
 #endif
-	//todo EEPROM initialisation????
-		Serial1.print("EEPROM.length ");
-		Serial1.println(EEPROM.length());
 
-	//if (EEPROM.init() == EEPROM_OK) {
-	//	uint16 cnt;
-	//	EEPROM.count(&cnt);
-	//	if(cnt>=EEPROM.maxcount()) {
-	//		// tmp, WIP: format eeprom if full
-	//		EEPROM.format();
-	//	}
 		return true;
-	//}
-	//return false;
 }
 
 void hwReadConfigBlock(void *buf, void *addr, size_t length)
@@ -73,7 +61,6 @@ void hwWriteConfigBlock(void *buf, void *addr, size_t length)
 	while (length-- > 0) {
 		EEPROM.write(offs++, *src++);
 	}
-//	EEPROM.commit();
 }
 
 uint8_t hwReadConfig(const int addr)
@@ -120,61 +107,6 @@ int8_t hwSleep(const uint8_t interrupt1, const uint8_t mode1, const uint8_t inte
 }
 
 
-//void hwRandomNumberInit(void)
-//{
-//	// This function initializes the random number generator with a seed
-//	// of 32 bits.  This method is good enough to earn FIPS 140-2 conform
-//	// random data. This should reach to generate 32 Bit for randomSeed().
-//	uint32_t seed = 0;
-//	uint32_t timeout = millis() + 20;
-//
-//	// Trigger floating effect of an unconnected pin
-//	pinMode(MY_SIGNING_SOFT_RANDOMSEED_PIN, INPUT_PULLUP);
-//	pinMode(MY_SIGNING_SOFT_RANDOMSEED_PIN, INPUT);
-//	delay(10);
-//
-//	// Generate 32 bits of datas
-//	for (uint8_t i=0; i<32; i++) {
-//		const int pinValue = analogRead(MY_SIGNING_SOFT_RANDOMSEED_PIN);
-//		// Wait until the analog value has changed
-//		while ((pinValue == analogRead(MY_SIGNING_SOFT_RANDOMSEED_PIN)) && (timeout>=millis())) {
-//			seed ^= (millis() << i);
-//			// Check of data generation is slow
-//			if (timeout<=millis()) {
-//				// Trigger pin again
-//				pinMode(MY_SIGNING_SOFT_RANDOMSEED_PIN, INPUT_PULLUP);
-//				pinMode(MY_SIGNING_SOFT_RANDOMSEED_PIN, INPUT);
-//				// Pause a short while
-//				delay(seed % 10);
-//				timeout = millis() + 20;
-//			}
-//		}
-//	}
-//	randomSeed(seed);
-//
-////	// use internal temperature sensor as noise source
-////	adc_reg_map *regs = ADC1->regs;
-////	regs->CR2 |= ADC_CR2_TSVREFE;
-////	regs->SMPR1 |= ADC_SMPR1_SMP16;
-////
-////	uint32_t seed = 0;
-////	uint16_t currentValue = 0;
-////	uint16_t newValue = 0;
-////
-////	for (uint8_t i = 0; i < 32; i++) {
-////		const uint32_t timeout = hwMillis() + 20;
-////		while (timeout >= hwMillis()) {
-////			newValue = adc_read(ADC1, 16);
-////			if (newValue != currentValue) {
-////				currentValue = newValue;
-////				break;
-////			}
-////		}
-////		seed ^= ( (newValue + hwMillis()) & 7) << i;
-////	}
-////	randomSeed(seed);
-////	regs->CR2 &= ~ADC_CR2_TSVREFE; // disable VREFINT and temp sensor
-//}
 
 bool hwUniqueID(unique_id_t *uniqueID)
 {
@@ -186,52 +118,16 @@ uint16_t hwCPUVoltage(void)
 {
 	//Not yet implemented
 	return FUNCTION_NOT_SUPPORTED;
-
-	//adc_reg_map *regs = ADC1->regs;
-	//regs->CR2 |= ADC_CR2_TSVREFE; // enable VREFINT and temp sensor
-	//regs->SMPR1 =  ADC_SMPR1_SMP17; // sample rate for VREFINT ADC channel
-	//adc_calibrate(ADC1);
-//
-	//const uint16_t vdd = adc_read(ADC1, 17);
-	//regs->CR2 &= ~ADC_CR2_TSVREFE; // disable VREFINT and temp sensor
-	//return (uint16_t)(1200u * 4096u / vdd);
 }
 
 uint16_t hwCPUFrequency(void)
 {
 	return F_CPU/100000UL;
 }
-//https://github.com/stm32duino/Arduino_Core_STM32/issues/5#issuecomment-411868042
+
 int8_t hwCPUTemperature(void)
 {
-		//STM32ADC adc(ADC1);
-
-		//adc.begin(PB1);
-
-	//  adc.startConversion();
-		
-    //uint32_t v = adc.read();
-    //uint32_t vcc = adc.measureVcc();
-    //int16_t temp = adc.measureTemp();
-    //printf("ADC: %lu, vcc: %lumV temp: %dC\n", v, vcc, temp);
-    //delay(1000);
-
-//		return adc.measureTemp();
-
-	//adc_reg_map *regs = ADC1->regs;
-	//regs->CR2 |= ADC_CR2_TSVREFE; // enable VREFINT and Temperature sensor
-	//regs->SMPR1 |= ADC_SMPR1_SMP16 | ADC_SMPR1_SMP17;
-	//adc_calibrate(ADC1);
-//
-	////const uint16_t adc_temp = adc_read(ADC1, 16);
-	////const uint16_t vref = 1200 * 4096 / adc_read(ADC1, 17);
-	//// calibrated at 25°C, ADC output = 1430mV, avg slope = 4.3mV / °C, increasing temp ~ lower voltage
-	//const int8_t temp = static_cast<int8_t>((1430.0 - (adc_read(ADC1, 16) * 1200 / adc_read(ADC1,
-	//                                        17))) / 4.3 + 25.0);
-	//regs->CR2 &= ~ADC_CR2_TSVREFE; // disable VREFINT and temp sensor
-	//return (temp - MY_STM32F1_TEMPERATURE_OFFSET) / MY_STM32F1_TEMPERATURE_GAIN;
 	return FUNCTION_NOT_SUPPORTED;
-
 }
 
 uint16_t hwFreeMem(void)
@@ -244,7 +140,6 @@ uint16_t hwFreeMem(void)
 void hwWatchdogReset(void)
 {
 	IWatchdog.reload();
-	//NRF_WDT->RR[0] = WDT_RR_RR_Reload;
 }
 
 void hwReboot(void)
