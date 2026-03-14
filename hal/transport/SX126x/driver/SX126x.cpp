2f8d026bf18da1fb5a3304abb0a37da4ae4e8a1e
tekka <olivier@mysensors.org>
2026-03-14 13:37:12 +0100

    Restling patch

--- hal/transport/SX126x/driver/SX126x.cpp
+++ hal/transport/SX126x/driver/SX126x.cpp
@@ -204,7 +204,8 @@ static void SX126x_handle()
 				sx126x_rxBufferStatus_t bufferStatus;
 				sx126x_packetStatus_t packetStatus;
 				SX126x_readCommand(SX126x_GET_RXBUFFERSTATUS, bufferStatus.values, 2);
-				bufferStatus.fields.payloadLength = min(bufferStatus.fields.payloadLength, (uint8_t)SX126x_MAX_PACKET_LEN);
+				bufferStatus.fields.payloadLength = min(bufferStatus.fields.payloadLength, 
+														(uint8_t)SX126x_MAX_PACKET_LEN);
 				SX126x.currentPacket.payloadLen = bufferStatus.fields.payloadLength - SX126x_HEADER_LEN;
 				SX126x_readBuffer(
 				    bufferStatus.fields.startPointer,
