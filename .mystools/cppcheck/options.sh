8f98d0007bd683d8c1c0ff01d32ac8d016e828d0
leniwiec <52417142+CodeGarageIo@users.noreply.github.com>
2026-04-13 10:09:38 +0200

    Revert files to merge-base HEAD state

--- .mystools/cppcheck/options.sh
+++ .mystools/cppcheck/options.sh
@@ -8,7 +8,7 @@ OPTIONS="--quiet                                          \
 	--language=c++                                        \
 	--library=${LIBRARY:-avr}                             \
 	--platform="${TOOLCONFIG}"/${PLATFORM:-avr.xml}       \
-	--includes-file="${TOOLCONFIG}"/includes.cfg	      \
+ 	--includes-file="${TOOLCONFIG}"/includes.cfg	      \
  	--inline-suppr                                        \
 	--suppressions-list="${TOOLCONFIG}"/suppressions.cfg"
 
